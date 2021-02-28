ab8aaee65bb12143cb59a8d0ad8df2bdc68c8983
Anubhav Sharma <anubhavs9@outlook.com>
2021-02-28 21:09:58 +0530

    Day1

--- 2/first.js
+++ 2/first.js
@@ -1,2 +1,8 @@
-var user_input = prompt("Enter name : ")
-document.getElementById("my_output").innerHTML = "Hello " + user_input;
+var Thing = prompt("Enter a thing name: ")
+var Meal = prompt("Enter a meal name(breakfast/lunch/dinner): ")
+
+var out = "Rosed are Red. Violets are Blue </br>"
+out += Thing + " are cheaper.  </br>"
+out += "than a  " + Meal + " for two."
+
+document.getElementById("out").innerHTML = out;
---
+++ 3/first.js
@@ -0,0 +1,11 @@
+var age = 45
+var status = -30
+var gpa = 4.0
+var out = ""
+out += "Age : " + age
+out += "<br/>"
+out += "Status : " + status
+out += "<br/>"
+out += "Gpa : " + gpa
+
+document.getElementById("my_output").innerHTML = out
